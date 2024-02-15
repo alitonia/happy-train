32877f16ef2cefb32daa19a59aab005fdd9018be
alitonia <allyofjustice1@gmail.com>
2024-02-15 17:56:44 +0700

    Add more infos

--- scripts/train-run-text.js
+++ scripts/train-run-text.js
@@ -1,21 +1,35 @@
 console.log("OK")
 
 var slots = [
-    "You",
-    "are",
-    "invited",
-    "to"
+    "Mời",
+    "mọi người",
+    "đến tham dự",
+    "lễ chụp ảnh của Chi"
 ]
 
 var visibleSlots = []
 var currentPhrase = ""
 var smokeEventName = "smoke_to_the_air"
+var finishEventName = "no_more_smoke_needs"
 
 var smoke = document.querySelector(".smoke")
 var msg = document.querySelector(".message")
+var moreMsg = document.querySelector(".additional-info-message")
 
 msg.addEventListener(smokeEventName, function (e) {
-    msg.innerText = e.detail
+    msg.innerHTML = e.detail
+})
+
+moreMsg.addEventListener(finishEventName, function (e) {
+    if (moreMsg.classList.contains("hidden-1")) {
+        moreMsg.classList.remove('hidden-1')
+    } else if (moreMsg.classList.contains("hidden-2")) {
+        moreMsg.classList.remove('hidden-2')
+    } else if (moreMsg.classList.contains("hidden-3")) {
+        moreMsg.classList.remove('hidden-3')
+    } else {
+        moreMsg.classList.remove('hidden-4')
+    }
 })
 
 smoke.addEventListener('animationiteration', function (e) {
@@ -26,5 +40,10 @@ smoke.addEventListener('animationiteration', function (e) {
             detail: currentPhrase
         })
         msg.dispatchEvent(cEvent)
+    } else {
+        var cEvent = new CustomEvent(finishEventName, {
+            detail: currentPhrase
+        })
+        moreMsg.dispatchEvent(cEvent)
     }
 })
